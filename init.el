f4cd906a34cef2c97f0d9658dd9bb2337de59404
vsarchelu <Herman.Lundkvist@vector.com>
2020-03-25 18:22:09 +0100

    added company

--- init.el
+++ init.el
@@ -53,7 +53,7 @@ There are two things you can do about this warning:
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (projectile fill-column-indicator yasnippet volatile-highlights helm-gtags evil company clojure-mode))))
+    (helm-projectile zzz-to-char projectile fill-column-indicator yasnippet volatile-highlights helm-gtags evil company clojure-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
