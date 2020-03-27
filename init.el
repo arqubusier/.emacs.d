e959b537e4cdea6a9a1e0a671b6735796ac2a077
Herman Lundkvist <herman.lundkvist@gmail.com>
2020-03-27 18:54:09 +0100

    helm-projectile config

--- init.el
+++ init.el
@@ -72,3 +72,9 @@ There are two things you can do about this warning:
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (projectile-mode +1)
+
+;;
+;; Helm-projectile
+;;
+(require 'helm-projectile)
+(helm-projectile-on)
